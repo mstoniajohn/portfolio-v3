a66954ca0e136e21ce274461fa011488f8fa419c
github username <github email address>
2021-05-02 22:16:18 -0400

    added header

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -12,8 +12,8 @@
 			</div>
 		</div>
 		<!-- 2 -->
-		<div class="shadow-md bg-blue-200 p-10 text-center rounded">
-			<h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+		<div class="shadow-md bg-gray-200 p-10 text-center rounded">
+			<h3 class="text-3xl text-gray-900 font-bold mb-4">Deaths</h3>
 			<div class="text-2xl mb-4">
 				<span class="font-bold"> New Deaths: </span>
 				{{ stats && formatNumber(stats.NewDeaths) }}
@@ -22,8 +22,12 @@
 				<span class="font-bold"> Total Deaths: </span>
 				{{ stats && formatNumber(stats.TotalDeaths) }}
 			</div>
+		</div>
+		<div class="shadow-md bg-green-200 p-10 text-center rounded">
+			<h3 class="text-3xl text-green-900 font-bold mb-4">Recovered</h3>
+
 			<div class="text-2xl mb-4">
-				<span class="font-bold"> Total Recover: </span>
+				<span class="font-bold"> Total Recovered: </span>
 				{{ stats && formatNumber(stats.TotalRecovered) }}
 			</div>
 		</div>
